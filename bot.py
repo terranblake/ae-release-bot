d85aa1f14e607693a46664a10641d6205a4bfbee
Terran Blake <terranblake@gmail.com>
2020-04-22 20:13:58 -0700

    check if user has already registered

--- bot.py
+++ bot.py
@@ -6,6 +6,15 @@ import time
 from flask import Flask
 from dotenv import load_dotenv
 
+alphatable = 'alphaentries'
+
+import sqlite3
+
+conn = sqlite3.connect('alphausers.db')
+c = conn.cursor()
+c.execute(f'CREATE TABLE IF NOT EXISTS {alphatable} (name text, discordid text, email text)')
+c.close()
+
 from splinter import Browser
 
 load_dotenv()
@@ -18,7 +27,7 @@ app.config["DEBUG"] = True
 
 TOKEN = os.getenv('DISCORD_TOKEN')
 DISCORD_GUILD = os.getenv('DISCORD_GUILD')
-member_name = 'TE-renBLAYK'
+admin_names = ['TE-renBLAYK']
 
 email_regex = r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
 
@@ -35,12 +44,16 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	print(f'''received message {message.content} from member {message.author.name}''')
+	print(f'''received message {message.content} from member {message.author.name} with id {message.author.id}''')
 
 	command, email = str(message.content).split(' ')
 	if command == '!getalpha' and re.search(email_regex, email):
 		print(f'''member {message.author.name} has requested to join the alpha using email {email}''')
-		getalpha(email)
+		res = getalpha(message.author.id, message.author.name, email)
+
+		if res is not False:
+			channel = await message.author.create_dm()
+			await channel.send(res)
 
 
 oculus_email_path = '//*[@id="email"]'
@@ -51,13 +64,25 @@ oculus_add_users_email_path = '//*[@id="user-value"]'
 oculus_add_users_rc_consent_path = '//*[@id="bxModal_section"]/form/div[3]/div[1]/div/div/div/label/span[1]'
 oculus_add_users_button_path = '//*[@id="bxModal_section"]/form/div[3]/div[2]/button'
 
-def getalpha(email):
+def getalpha(discordid, name, email):
+	c = conn.cursor()
+
+	c.execute(f'SELECT * FROM {alphatable} WHERE discordid="{discordid}";')
+	rows = c.fetchall()
+	if len(rows) > 0:
+		c.close()
+		entry = rows[0]
+		print(entry)
+		return f'user with discord id {entry[0]} and name {entry[1]} already registered using email {entry[2]}'
+
 	print(f'''giving access to {email} for alpha''')
 	browser = Browser(driver_name='chrome')
 
 	# credentials for logging into oculus site
 	OCULUS_EMAIL = os.getenv('OCULUS_EMAIL')
 	OCULUS_PASSWORD = os.getenv('OCULUS_PASSWORD')
+
+	OCULUS_BUILD_URL = os.getenv('OCULUS_BUILD_URL')
 
 	try:
 		if not OCULUS_EMAIL:
@@ -66,25 +91,23 @@ def getalpha(email):
 		if not OCULUS_PASSWORD:
 			raise Exception('missing oculus password in env') 
 	except Exception:
-		print('oops! there were missing credentials. bailing!')
 		browser.quit()
-		return
+		return 'oops! there were missing credentials. bailing!'
 
-	# 
+	# oculus developer's console login
 	browser.visit(f'https://auth.oculus.com/login-without-facebook/?redirect_uri=https%3A%2F%2Fdashboard.oculus.com%2Fredirect%2F')
 	browser.find_by_xpath(oculus_email_path).fill(OCULUS_EMAIL)
 	browser.find_by_xpath(oculus_password_path).fill(OCULUS_PASSWORD)
-
 	login_button = browser.find_by_xpath(oculus_login_button_path)
 	login_button.click()
 
+	# wait for login to finish
 	time.sleep(5)
 
 	try:
 
-		# navigate to builds page
-		browser.visit('https://dashboard.oculus.com/application/1631246303646358/build/')
-
+		# navigate to builds page for specific project
+		browser.visit(OCULUS_BUILD_URL)
 		time.sleep(5)
 
 		# click the add users button
@@ -103,8 +126,19 @@ def getalpha(email):
 	except Exception as e:
 		print(f'there was a problem adding user to oculus list')
 		print(e)
+		browser.quit()
+		return e
+
+	c = conn.cursor()
+	# Insert a row of data
+	c.execute(f"INSERT INTO {alphatable} VALUES ('{name}','{discordid}','{email}')")
+
+	# Save (commit) the changes
+	conn.commit()
+	conn.close()
 
 	browser.quit()
+	return False
 
 
 client.run(TOKEN)
